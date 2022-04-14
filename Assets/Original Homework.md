bc95c3b54d533d0c1f49f2582ba3bfad8256a8be
Fernando Vasquez <vasquezjrf@gmail.com>
2022-04-13 20:50:41 -0400

    updated caching in service worker

--- Assets/Original Homework.md
+++ Assets/Original Homework.md
@@ -34,10 +34,13 @@ THEN I find that my JavaScript files have been bundled using webpack -->
 <!-- WHEN I run my webpack plugins
 THEN I find that I have a generated HTML file, service worker, and a manifest file -->
 
-WHEN I use next-gen JavaScript in my application
-THEN I find that the text editor still functions in the browser without errors
+<!-- WHEN I use next-gen JavaScript in my application
+THEN I find that the text editor still functions in the browser without errors -->
+
+<!--
 WHEN I open the text editor
-THEN I find that IndexedDB has immediately created a database storage
+THEN I find that IndexedDB has immediately created a database storage -->
+
 WHEN I enter content and subsequently click off of the DOM window
 THEN I find that the content in the text editor has been saved with IndexedDB
 WHEN I reopen the text editor after closing it
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,6 +17,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  // export const putDb = async () => {
   console.log("Posting to the database");
 
   //create a connection to our database and the version of the database we wish to use
@@ -44,7 +45,7 @@ export const putDb = async (content) => {
 // export const getDb = async () => console.error("getDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (content) => {
   console.log("Getting information from the database");
 
   //create a connection to the DB and the version of the DB we wish to use
@@ -57,7 +58,7 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
 
   //get ALL data from the database
-  const request = store.getAll();
+  const request = store.getAll(content);
 
   //get confirmation of the request to the database
   const result = await request;
